4523c816952d086eb8daac05ddc00465cc7c3916
Michal Podhradsky <michal.podhradsky@aggiemail.usu.edu>
2017-08-18 18:39:04 -0700

    Now it works!

--- apps/rumprun_rust/components/hellorust/rustest/src/main.rs
+++ apps/rumprun_rust/components/hellorust/rustest/src/main.rs
@@ -3,6 +3,13 @@
 fn main() {
     println!("Hello rust!");
     unsafe {
+    *(*camkes_buffer).offset(0) = 'a' as u8;
+    *(*camkes_buffer).offset(1) = 'b' as u8;
+    *(*camkes_buffer).offset(2) = 'c' as u8;
+    *(*camkes_buffer).offset(3) = '\0' as u8;
+			println!("RUST: Buffer: {}", *(*camkes_buffer).offset(0) as char);
+			println!("RUST: Buffer: {}", *(*camkes_buffer).offset(1) as char);
+			println!("RUST: Buffer: {}", *(*camkes_buffer).offset(2) as char);
         (*((&camkes_ev_emit as *const *const extern "C" fn()) as *const extern "C" fn()))();
     }
     println!("Waiting for event!");
@@ -10,6 +17,11 @@ fn main() {
         std::mem::transmute::<*const extern "C" fn(), extern "C" fn()>(camkes_ev1_wait)();
     }
     println!("Got event!");
+    unsafe {
+			println!("RUST: Buffer: {}", *(*camkes_buffer).offset(0) as char);
+			println!("RUST: Buffer: {}", *(*camkes_buffer).offset(1) as char);
+			println!("RUST: Buffer: {}", *(*camkes_buffer).offset(2) as char);
+    }
 
 }
 
@@ -20,7 +32,7 @@ extern "C" {
     #[linkage = "extern_weak"]
     static camkes_ev1_wait: *const extern "C" fn();
 
-	#[no_mangle]
-	static camkes_buffer: *mut [u8];
+#[linkage = "extern_weak"]
+	static camkes_buffer: *const *mut u8;
     
 }
