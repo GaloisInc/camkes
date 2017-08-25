5d251b532d83fdacd6c01fce6107e88f9213e1db
Michal Podhradsky <michal.podhradsky@aggiemail.usu.edu>
2017-08-25 11:27:47 -0700

    An example rust ethernet - so far only UDP frames, notes added about raw sockets on BSD

--- apps/rumprun_ethernet_rust/components/rump_ether/ethdriver/src/main.rs
+++ apps/rumprun_ethernet_rust/components/rump_ether/ethdriver/src/main.rs
@@ -1,46 +1,5 @@
 #![feature(linkage)]
 
-use std::io::prelude::*;
-use std::net::{TcpListener, TcpStream};
-
-fn main() {
-    println!("TCP listener starting!");
-    let listener = TcpListener::bind("192.168.179.50:6666").unwrap();
-
-	println!("Waiting for connection");
-  match listener.accept() {
-    Ok((socket, addr)) => {
-      println!("new client: {:?}", addr);
-      handle_client(socket);
-    },
-    Err(e) => println!("couldn't get client: {:?}", e), 
-  }
-
-    println!("Rust done");
-}
-
-fn handle_client(mut stream: TcpStream) {
-  let mut buf: Vec<u8> = vec![0; 255];
-  let len = stream.read(buf.as_mut_slice()).unwrap();
-
-  unsafe {
-    for idx in 0..len {
-      *(*eth_buffer).offset(idx as isize) = buf[idx] as u8;
-    }
-
-    println!("camkes_ev_emit!");
-    (*((&eth_ev_rx_emit as *const *const extern "C" fn()) as *const extern "C" fn()))();
-    println!("camkes ev wait!");
-        //std::mem::transmute::<*const extern "C" fn(), extern "C" fn()>(camkes_ev1_wait)();
-    (*((&eth_ev_tx_wait as *const *const extern "C" fn()) as *const extern "C" fn()))();
-        
-        for idx in 0..len as isize {
-        	println!("RUST: Buffer: {}", *(*eth_buffer).offset(idx) as char);
-        }
-  }
-
-}
-
 #[no_mangle]
 extern "C" {
     #[linkage = "extern_weak"]
@@ -50,3 +9,201 @@ extern "C" {
     #[linkage = "extern_weak"]
     static eth_buffer: *const *mut u8;
 }
+
+/*
+// fails because it needs libc definitions such as 
+error[E0432]: unresolved import `self::libc::SOCK_NONBLOCK`
+  --> /home/podhrad/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy-socket-0.2.1/src/raw/unix.rs:79:9
+   |
+79 |         SOCK_NONBLOCK,
+   |         ^^^^^^^^^^^^^ no `SOCK_NONBLOCK` in the root. Did you mean to use `O_NONBLOCK`?
+
+error[E0432]: unresolved import `self::libc::SOCK_CLOEXEC`
+  --> /home/podhrad/.cargo/registry/src/github.com-1ecc6299db9ec823/lazy-socket-0.2.1/src/raw/unix.rs:80:9
+   |
+80 |         SOCK_CLOEXEC
+   |         ^^^^^^^^^^^^ no `SOCK_CLOEXEC` in the root. Did you mean to use `O_CLOEXEC`?
+
+error: aborting due to 2 previous errors
+
+-> maybe implement netbsd version? 
+https://github.com/DoumanAsh/lazy-socket.rs/blob/master/src/raw/mod.rs
+*/
+//extern crate lazy_socket;
+//
+//use std::net;
+//use std::str::FromStr;
+//
+//use lazy_socket::raw::{
+//    Socket,
+//    Family,
+//    Protocol,
+//    Type,
+//    select
+//};
+//
+//fn main() {
+//    let timeout = 1000;
+//    let socket = match Socket::new(Family::IPv4, Type::STREAM, Protocol::TCP) {
+//        Ok(socket) => socket,
+//        Err(error) => {
+//            println!("Couldn't open socket. Erro: {}", error);
+//            return;
+//        }
+//    };
+//
+//    let dest = net::SocketAddr::from_str("192.168.0.1:80").unwrap();
+//
+//    let _ = socket.set_blocking(false);
+//    let _ = socket.connect(&dest);
+//    match select(&[], &[&socket], &[&socket], Some(timeout)) {
+//          Ok(_) => println!("Connected!"),
+//          Err(error) => println!("Failed to connect. Error:{}", error)
+//    }
+//}
+
+
+/*
+fails on libc defines too:
+
+error[E0425]: cannot find value `AF_PACKET` in module `libc`
+  --> /home/podhrad/.cargo/registry/src/github.com-1ecc6299db9ec823/smoltcp-0.3.0/src/phy/sys/raw_socket.rs:14:44
+   |
+14 |             let lower = libc::socket(libc::AF_PACKET, libc::SOCK_RAW,
+   |                                            ^^^^^^^^^ not found in `libc`
+
+error[E0433]: failed to resolve. Use of undeclared type or module `imp`
+  --> /home/podhrad/.cargo/registry/src/github.com-1ecc6299db9ec823/smoltcp-0.3.0/src/phy/sys/raw_socket.rs:15:38
+   |
+15 |                                      imp::ETH_P_ALL.to_be() as i32);
+   |                                      ^^^ Use of undeclared type or module `imp`
+
+error[E0433]: failed to resolve. Use of undeclared type or module `imp`
+  --> /home/podhrad/.cargo/registry/src/github.com-1ecc6299db9ec823/smoltcp-0.3.0/src/phy/sys/raw_socket.rs:27:50
+   |
+27 |         ifreq_ioctl(self.lower, &mut self.ifreq, imp::SIOCGIFMTU).map(|mtu| mtu as usize)
+   |                                                  ^^^ Use of undeclared type or module `imp`
+
+error[E0422]: cannot find struct, variant or union type `sockaddr_ll` in module `libc`
+  --> /home/podhrad/.cargo/registry/src/github.com-1ecc6299db9ec823/smoltcp-0.3.0/src/phy/sys/raw_socket.rs:31:30
+   |
+31 |         let sockaddr = libc::sockaddr_ll {
+   |                              ^^^^^^^^^^^ did you mean `sockaddr_in`?
+
+error[E0425]: cannot find value `AF_PACKET` in module `libc`
+  --> /home/podhrad/.cargo/registry/src/github.com-1ecc6299db9ec823/smoltcp-0.3.0/src/phy/sys/raw_socket.rs:32:33
+   |
+32 |             sll_family:   libc::AF_PACKET as u16,
+   |                                 ^^^^^^^^^ not found in `libc`
+
+error[E0433]: failed to resolve. Use of undeclared type or module `imp`
+  --> /home/podhrad/.cargo/registry/src/github.com-1ecc6299db9ec823/smoltcp-0.3.0/src/phy/sys/raw_socket.rs:33:27
+   |
+33 |             sll_protocol: imp::ETH_P_ALL.to_be() as u16,
+   |                           ^^^ Use of undeclared type or module `imp`
+
+error[E0433]: failed to resolve. Use of undeclared type or module `imp`
+  --> /home/podhrad/.cargo/registry/src/github.com-1ecc6299db9ec823/smoltcp-0.3.0/src/phy/sys/raw_socket.rs:34:73
+   |
+34 |             sll_ifindex:  try!(ifreq_ioctl(self.lower, &mut self.ifreq, imp::SIOCGIFINDEX)),
+   |                                                                         ^^^ Use of undeclared type or module `imp`
+
+error[E0412]: cannot find type `sockaddr_ll` in module `libc`
+  --> /home/podhrad/.cargo/registry/src/github.com-1ecc6299db9ec823/smoltcp-0.3.0/src/phy/sys/raw_socket.rs:43:60
+   |
+43 |                                  &sockaddr as *const libc::sockaddr_ll as *const libc::sockaddr,
+   |                                                            ^^^^^^^^^^^ did you mean `sockaddr_in`?
+
+error[E0412]: cannot find type `sockaddr_ll` in module `libc`
+  --> /home/podhrad/.cargo/registry/src/github.com-1ecc6299db9ec823/smoltcp-0.3.0/src/phy/sys/raw_socket.rs:44:55
+   |
+44 |                                  mem::size_of::<libc::sockaddr_ll>() as u32);
+   |                                                       ^^^^^^^^^^^ did you mean `sockaddr_in`?
+
+error: aborting due to 9 previous errors
+
+error: Could not compile `smoltcp`.
+
+*/
+//extern crate smoltcp;
+//
+//use std::env;
+//use smoltcp::phy::{Device, RawSocket};
+//use smoltcp::wire::{PrettyPrinter, EthernetFrame};
+//
+//fn main() {
+//    let ifname = env::args().nth(1).unwrap();
+//    let mut socket = RawSocket::new(ifname.as_ref()).unwrap();
+//    loop {
+//        let buffer = socket.receive(/*timestamp=*/0).unwrap();
+//        print!("{}", PrettyPrinter::<EthernetFrame<&[u8]>>::new("", &buffer))
+//    }
+//}
+
+
+// try with nix -> raw sockets dont work, we use regular UDP sockets for start (and that works)
+extern crate nix;
+extern crate rand;
+
+use nix::sys::socket::*;
+
+fn main() {
+	let fd = socket(AddressFamily::Inet, SockType::Datagram, SockFlag::empty(), IPPROTO_UDP).expect("Unable to create socket");
+	println!("Socket created, fd={}",fd);
+	
+	let mut v: Vec<u8> = vec![0;255];
+	println!("Just created vector {} bytes long",v.len());
+	
+	// bind socket
+	match bind(fd, &SockAddr::new_inet(InetAddr::new(IpAddr::new_v4(192,168,179,50), 6666))) {
+			Ok(_) => println!("Bind OK!"),
+			Err(e) => println!("error binding socket: {}",e),
+		}
+	
+	// no raw sockets in netbd?
+	let len = recv(fd, &mut v, MsgFlags::empty()).expect("Error receiving data");
+	
+	println!("Either way, here is the buffer {:?}",v);
+	
+	// copy the buffer
+	unsafe {
+		for idx in 0..len{
+			 *(*eth_buffer).offset(idx as isize) = v[idx];
+		}
+	}
+	
+	// notify the ipstack
+	unsafe {
+	    (*((&eth_ev_rx_emit as *const *const extern "C" fn()) as *const extern "C" fn()))();	
+    }
+	
+	
+}
+
+
+// an innocent main
+//fn main() {
+//	println!("Hello rust");
+//}
+
+
+// try with nix -> use bfp
+// We get "no such file or directory problem
+// see http://infosecwriters.com/text_resources/pdf/raw_tcp.pdf
+//extern crate nix;
+//
+//use nix::sys::ioctl::*;
+//use nix::fcntl::*;
+//use nix::sys::stat::Mode;
+//
+//
+//fn main() {
+//	// "/dev/bpfX"
+//	// fd = open((char *) &buf, O_RDWR);
+//	let path = "/dev/bpf0";
+//	let fd = open(path, O_RDWR, Mode::empty());
+//	match fd {
+//		Ok(fd) => println!("File opened, fd={}",fd),
+//		Err(e) => println!("error opening file: {}",e),
+//	}
+//}
--- apps/rumprun_ethernet_rust/components/rump_ipstack/ipstack/src/main.rs
+++ apps/rumprun_ethernet_rust/components/rump_ipstack/ipstack/src/main.rs
@@ -24,9 +24,52 @@ fn main() {
     println!("Hello ipstack!");
     println!("Waiting for ipframe_ev_tx!");
     unsafe {
-    (*((&eth_ev_rx_wait as *const *const extern "C" fn()) as *const extern "C" fn()))();	
+	    (*((&eth_ev_rx_wait as *const *const extern "C" fn()) as *const extern "C" fn()))();	
     }
     
+    let mut len = 0;
+    // copy to a buffer
+    unsafe {
+    	let mut idx: isize = 0;
+    	let mut val = *(*eth_buffer).offset(0) as u8;
+    	
+    	while val!=0 {
+    		*(*network_buffer).offset(idx) = val;
+    		
+    		idx +=1;
+    		val = *(*eth_buffer).offset(idx) as u8;
+    	}
+    	
+    	len = idx;
+    }
+
+	// display
+    let mut msg = String::new();
+    for idx in 0..len {
+    	unsafe {
+    		msg.push(*(*network_buffer).offset(idx) as char);
+    	}
+    }    
+    println!("Got data! {}",msg);
+    
+    // notify receiver
+    println!("Notifying string_reverse");
+    unsafe {
+	    (*((&ipframe_ev_rx_emit as *const *const extern "C" fn()) as *const extern "C" fn()))();	
+    }
+    
+    println!("waiting for response");
+    unsafe {
+	    (*((&ipframe_ev_tx_wait as *const *const extern "C" fn()) as *const extern "C" fn()))();	
+    }
+    
+    let mut reversed_msg = String::new();
+    for idx in 0..len {
+    	unsafe {
+    		reversed_msg.push(*(*network_buffer).offset(idx) as char);
+    	}
+    }
+    println!("Received {}", reversed_msg);
     
 }
 
