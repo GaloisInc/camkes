421a946f9e0617c1c634dba11fba31b372421382
Michal Podhradsky <michal.podhradsky@aggiemail.usu.edu>
2017-09-05 11:40:12 -0700

    Passthrough rust_ether component

--- apps/rumprun_ethernet_rust/components/rump_ether/ethdriver/src/main.rs
+++ apps/rumprun_ethernet_rust/components/rump_ether/ethdriver/src/main.rs
@@ -4,10 +4,12 @@
 extern "C" {
     #[linkage = "extern_weak"]
     static eth_ev_rx_emit: *const extern "C" fn();
+    
     #[linkage = "extern_weak"]
-    static eth_ev_tx_wait: *const extern "C" fn();
+    static eth_ev_tx_poll: *const extern "C" fn();
+    
     #[linkage = "extern_weak"]
-    static eth_buffer: *const *mut u8; // TODO: use a better struct, maybe something with length?
+    static eth_buffer: *const *mut u8; // length is stored as the first byte of the buffer
 }
 
 // TODO: Should have two scokets and some mutex over the data buffer
@@ -22,10 +24,14 @@ fn emit_rx_data() {
 }
 
 // Wait for data to send
-// TODO: how do I know how much data?
-fn wait_tx_data() {
-    unsafe {
-        (*((&eth_ev_tx_wait as *const *const extern "C" fn()) as *const extern "C" fn()))();
+fn poll_tx_data() -> bool {
+    let res = unsafe {
+        (*((&eth_ev_tx_poll as *const *const extern "C" fn()) as *const extern "C" fn() -> i32))()
+    };
+    match res {
+        0 => false,
+        1 => true,
+        _ => panic!("Unexpected return code res={}",res),
     }
 }
 
@@ -33,83 +39,87 @@ fn wait_tx_data() {
 fn copy_data_from(buf: &[u8], len: usize) {
     // TODO: check for mutex
     unsafe {
+        *(*eth_buffer).offset(0) = len as u8;
         for idx in 0..len {
-            *(*eth_buffer).offset(idx as isize) = buf[idx];
+            *(*eth_buffer).offset(idx as isize + 1) = buf[idx];
         }
     }
 }
 
-fn copy_data_to(buf: &mut [u8], len: usize) {
+fn copy_data_to(buf: &mut Vec<u8>) -> usize {
     // TODO: check for mutex
     unsafe {
-        for idx in 0..len {
+        let len = *(*eth_buffer).offset(0) as usize;
+        for idx in 1..len + 1 {
             buf[idx] = *(*eth_buffer).offset(idx as isize);
         }
+        len + 1
     }
 }
 
 
 // UDP Socket
 use std::net::UdpSocket;
-use std::{thread,time};
+use std::thread;
 
-
+// Note: sleep suspends whole Rumpkernel component, not cool...
 fn main() {
-
-
-
     println!("Spwaning thread RX");
     // reads data from the socket
     let trx = thread::spawn(move || {
-        println!("Waiting for a socket to bind");
+        println!("RX Waiting for a socket to bind");
         // bind to an IP address assigned to an existing interface & specific port
         let socket = UdpSocket::bind("192.168.179.50:6666").expect("couldn't bind to address");
-        // read from the socket
+        socket
+            .set_nonblocking(true)
+            .expect("set_nonblocking call failed");
+
+        // allocate buffer
         let mut buf: Vec<u8> = vec![0; 1500];
 
+        println!("RX entering main loop");
         loop {
-            let (len, src) = socket.recv_from(&mut buf).expect("Didn't receive data");
-            println!("Received {} data from {}", len, src);
+            if let Ok(res) = socket.recv_from(&mut buf) {
+                let (len, src) = res;
+                println!("Received {} data from {}: {:?}", len, src, &buf[0..len]);
 
-            copy_data_from(&buf, len);
+                copy_data_from(&buf, len);
 
-            emit_rx_data();
+                emit_rx_data();
+            }
+            thread::yield_now();
         }
     });
 
     println!("Spwaning thread TX");
     // waits for data to be sned
     let ttx = thread::spawn(move || {
+        println!("TX Waiting for a socket to bind");
         // bind to an IP address assigned to an existing interface & specific port
         let socket = UdpSocket::bind("192.168.179.50:6665").expect("couldn't bind to address");
+        socket
+            .set_nonblocking(true)
+            .expect("set_nonblocking call failed");
+
+        // allocate buffer
         let mut buf: Vec<u8> = vec![0; 1500];
 
+
+        println!("TX entering main loop");
         loop {
-            println!("Waiting for data to send");
-            wait_tx_data(); // this freezes execution of whole rumpkernel :-(
-            println!("TX data received");
+            if poll_tx_data() {
+                let msg_len = copy_data_to(&mut buf);
 
-            // TODO: figure lenght
-            copy_data_to(buf.as_mut_slice(), 255);
-
-            // send data
-            let len = socket
-                .send_to(&buf, "192.168.179.1:6665")
-                .expect("Couldn't send data");
-            println!("Sent {} bytes", len);
+                println!("TX sending data: {:?}", &buf[1..msg_len]);
+                // send data
+                let len = socket
+                    .send_to(&buf[1..msg_len], "192.168.179.1:6665")
+                    .expect("Couldn't send data");
+                println!("Sent {} bytes", len);
+            }
+            thread::yield_now();
         }
     });
-    
-    println!("Launching Thread counter");
-    let tcnt = thread::spawn(move || {
-    		let mut counter = 0;
-    		loop {
-    			println!("Time is {} [s]",counter);
-    			counter += 1;
-    			thread::sleep(time::Duration::from_millis(1000));
-    		}
-    	}
-    );
 
     println!("Waiting for threads to end");
     match trx.join() {
@@ -119,9 +129,5 @@ fn main() {
     match ttx.join() {
         Ok(_) => println!("Ttx All well"),
         Err(e) => println!("Ttx thread joined with an error {:?}", e), 
-    }
-    match tcnt.join() {
-        Ok(_) => println!("Tcnt All well"),
-        Err(e) => println!("Tcnt thread joined with an error {:?}", e), 
     }
 }
